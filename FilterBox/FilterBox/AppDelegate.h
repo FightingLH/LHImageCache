28344459d593891f90bcc4fd38db7b22dda86db0
lh <feeyo@feeyodeMac-mini.local>
2018-05-16 14:51:19 +0800

    no message

---
+++ FilterBox/FilterBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FilterBox
+//
+//  Created by feeyo on 2018/5/16.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FilterBox/FilterBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FilterBox
+//
+//  Created by feeyo on 2018/5/16.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
